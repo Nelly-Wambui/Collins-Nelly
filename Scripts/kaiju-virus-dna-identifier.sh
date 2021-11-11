4ac2fac9cf3883c1fd34ea9137894cb59d12a84e
Nelly-Wambui <nellywambui1997@gmail.com>
2021-11-11 19:34:24 +0300

    Verification of virus dna

---
+++ Scripts/kaiju-virus-dna-identifier.sh
@@ -0,0 +1,11 @@
+path1=~/ncbi/mapped-sequences/unmapped-reads/unmapped-fastq
+for file in $(cat ../Raw-Data/SraAccList.txt)
+do
+mkdir $path1/kaiju/$file
+kaiju -t $path1/kaiju/bin/kaijuDB/nodes.dmp -f $path1/kaiju/bin/kaijuDB/viruses/kaiju_db_viruses.fmi -i $path1/$file-megahitout/final.contigs.fa -o $path1/kaiju/$file/$file.out
+grep 'C' $path1/kaiju/$file/$file.out | cut -f 2 > $path1/kaiju/$file/identifiers.txt
+for id in $(cat $path1/kaiju/$file/identifiers.txt)
+do
+grep -A1 $id $path1/$file-megahitout/final.contigs.fa > $path1/kaiju/virus-dna/$file.fasta
+done
+done 
