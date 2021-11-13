605ddf1999704457f6691c788277413c2860b3b9
ckigen.k <ckigen.k@gmail.com>
2021-11-14 01:15:48 +0300

    Updated script documentation

--- Scripts/blast-results.sh
+++ Scripts/blast-results.sh
@@ -1,6 +1,8 @@
+#Assign data directory to a variable
 path1=~/ncbi/mapped-sequences/unmapped-reads/unmapped-fastq/kaiju/virus-dna
-
+#Loops through each file
 for file in $(cat ../Raw-Data/SraAccList.txt)
 do
+#Performs a blastn search of each sequence and saves results to an output file
 blastn -db nt -query $path1/$file.fasta -out $path1/blast-results/$file.out -remote
 done 
